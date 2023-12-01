2390cbea58c42562ff626fb3ff937610baec4035
Arpit-Singh22 <arpitsingh22@hotmail.com>
2023-12-01 10:09:21 +0530

    Hello Canvas

---
+++ main.js
@@ -0,0 +1,77 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// adding directional light
+
+const color = 0xffffff;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+// const cubeMaterial = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
+
+// const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+// scene.add(cubeMesh);
+
+function makeInstance(geometry, color, x) {
+  const material = new THREE.MeshPhongMaterial({ color });
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  cube.position.x = x;
+  return cube;
+}
+
+const cubes = [
+  makeInstance(geometry, 0x44aa88, 0),
+  makeInstance(geometry, 0x8844aa, -2),
+  makeInstance(geometry, 0xaa8844, 2),
+];
+
+//initialize the camera
+const fov = 75; // radian
+const aspectRatio = window.innerWidth / window.innerHeight;
+const near = 0.1;
+const far = 30;
+const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+
+//position the camera
+camera.position.z = 5;
+
+//initialize the rederer
+const canvas = document.querySelector("canvas.threejs");
+const renderer = new THREE.WebGL1Renderer({ canvas: canvas });
+renderer.setSize(window.innerWidth, window.innerHeight, false);
+// renderer.render(scene, camera);
+
+// function render(time) {
+//   time *= 0.001; // convert time to seconds
+
+//   cubeMesh.rotation.x = time;
+//   cubeMesh.rotation.y = time;
+
+//   renderer.render(scene, camera);
+
+//   requestAnimationFrame(render);
+// }
+// requestAnimationFrame(render);
+
+function render(time) {
+  time *= 0.001; // convert time to seconds
+
+  cubes.forEach((cube, ndx) => {
+    const speed = 1 + ndx * 0.1;
+    const rot = time * speed;
+    cube.rotation.x = rot;
+    cube.rotation.y = rot;
+  });
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
